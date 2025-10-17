05927b82afcae197c542e82d04d468831affdd43
pinguuzin <reipinguimoficial@gmail.com>
2025-10-17 09:57:23 +0100

    Add language support and refactor config structure

--- config.lua
+++ config.lua
@@ -1,10 +1,45 @@
 Config = {}
 
-Config.Command = 'menuropa' -- Command to open the menu.
+-- Command and keybind
+Config.Command = 'menuropa'
+Config.KeyToOpen = 'F5'
 
-Config.KeyToOpen = 'F5' -- default key to open the menu (every player can change it in GTAV settings).
 
-Config.TakeOff = { -- The ID of the component that will be used when you take off the clothes (these are the vanilla IDs without clothing mods).
+
+-- Language (can change to "en" and "pt")
+Config.Language = 'en'
+
+
+
+-- DO NOT TOUCH
+Locales = {}
+
+local function LoadLocale(lang)
+    local path = string.format('locales/%s.json', lang)
+    print(('[MenuClothes] Trying to load language: %s'):format(lang))
+
+    local file = LoadResourceFile(GetCurrentResourceName(), path)
+    if file then
+        local data = json.decode(file)
+        if data then
+            print(('[MenuClothes] %s.json loaded successfully!'):format(lang))
+            return data
+        else
+            print(('[MenuClothes] Error decoding %s.json'):format(lang))
+        end
+    else
+        print(('[MenuClothes] File %s.json not found!'):format(lang))
+    end
+    return nil
+end
+
+Locales = LoadLocale(Config.Language) or LoadLocale('en') or {}
+
+
+
+-- Below we can configure the clothes you want to stay when you take them off in the menu if you have mods you have to change them.
+
+Config.TakeOff = {
     male = {    
         shirt = 15,
         pants = 21,
